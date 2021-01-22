c6b456e3439130a39b99cd76d7f8c7b16d77cddc
Nick Pandelakis <pandelakisn@carleton.edu>
2021-01-22 10:52:50 -0600

    revisions

--- books/books.py
+++ books/books.py
@@ -1,15 +1,13 @@
 '''
 books.py
 Written by Ross Grogan-Kaylor and Nick Pandelakis for cs257
-Revised by Nick Pandelakis for 1/22
+    Revised by Nick Pandelakis for 1/22
 A command line interface for searching the 'books.csv' file.
 '''
 
 import csv
 import argparse
 
-# Interesting: "-b", "--book" vs. "b, --book": "-b", "--book" is the one you want (treats -b and --book as referring to the
-# same argument); "-b, --book" treats -b and --book as referring to the same argument, but where -b can be specified with no args
 
 def get_parsed_arguments():
     # Set up command line arguments.
@@ -22,7 +20,6 @@ def get_parsed_arguments():
     parser.add_argument("-a", "--authors", nargs="+",
                         help="One or more substrings to search for in the names of authors. If one of the substrings contains "
                              "a space, surround that substring with quotes \"\".")
-    # may need to fix, see python3 books.py books.csv -b 'the' 1800 1899 for example
     parser.add_argument("year1", nargs = "?", help="One of the years in the time "
                                                                  "interval [min(year1, year2), max(year1, year2)] "
                                                                  "within which to search for books.")
@@ -33,27 +30,27 @@ def get_parsed_arguments():
     parsed_arguments = parser.parse_args()
 
     # Handle the years.
+    # Note that year1 or year2 might still be None.
     year1 = parsed_arguments.year1
     if parsed_arguments.year2 is None:
         parsed_arguments.year2 = year1
 
-    # Note that year1 or year2 might still be None.
     return parsed_arguments 
 
 
 def filter_books(filtered, books) -> list:
     title_index = 0
-    return list(filter(lambda p: any(sub.lower() in p[title_index].lower() for sub in books), filtered))
+    return list(filter(lambda f: any(substring.lower() in f[title_index].lower() for substring in books), filtered))
 
 
 def filter_authors(filtered, authors) -> list: 
     author_index = 2
-    return list(filter(lambda p: any(sub.lower() in p[author_index].lower() for sub in authors), filtered))
+    return list(filter(lambda f: any(substring.lower() in f[author_index].lower() for substring in authors), filtered))
 
 
 def filter_years(filtered, year1, year2) -> list:
     year_index = 1
-    return list(filter(lambda p: year1 <= p[year_index] and year2 >= p[year_index], filtered))
+    return list(filter(lambda f: year1 <= f[year_index] and year2 >= f[year_index], filtered))
 
 
 def get_authorset(filtered, authors) -> set:
