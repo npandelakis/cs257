e262f16cadfec6654b1c0a0b7e4a35222e18f11d
Nick Pandelakis <pandelakisn@carleton.edu>
2021-01-27 12:52:31 -0600

    work for database design project

---
+++ database/convert.py
@@ -0,0 +1,170 @@
+'''
+convert.py
+This program converts one large kaggle csv into smaller csvs that correspond
+with a specific database.
+Written by Nick Pandelakis for cs257
+'''
+
+import csv
+
+
+def create_athletes_csv(reader_list):
+    writer = csv.writer(open('athletes.csv', 'w'))
+    name_index = 1 
+    i = 1
+    for row in reader_list[1:]:
+        if i == int(row[0]):
+            writer.writerow([i, row[name_index]])
+            i = i + 1
+
+
+def create_sports_csv(reader_list):
+    writer = csv.writer(open('sports.csv','w'))
+    sport_dict = {}
+    sport_index = 12
+    i = 1
+    for row in reader_list[1:]:
+        if row[sport_index] not in sport_dict:
+            sport_dict[row[sport_index]] = i
+            i = i + 1
+    for key in sport_dict:
+        writer.writerow([sport_dict[key], key])
+    
+def create_events_csv(reader_list):
+    writer = csv.writer(open('events.csv','w'))
+    event_dict = {}
+    event_index = 13
+    i = 1
+    for row in reader_list[1:]:
+        if row[event_index] not in event_dict:
+            event_dict[row[event_index]] = i
+            i = i + 1
+    for key in event_dict:
+        writer.writerow([event_dict[key], key])
+
+def create_teams_csv(reader_list):
+    writer = csv.writer(open('teams.csv','w'))
+    team_dict = {}
+    team_index = 6
+    i = 1
+    for row in reader_list[1:]:
+        if row[team_index] not in team_dict:
+            team_dict[row[team_index]] = i
+            i = i + 1
+    for key in team_dict:
+        writer.writerow([team_dict[key], key])
+
+def create_nocs_csv(reader_list): 
+    writer = csv.writer(open('nocs.csv','w'))
+    noc_dict = {}
+    noc_index = 7
+    i = 1
+    for row in reader_list[1:]:
+        if row[noc_index] not in noc_dict:
+            noc_dict[row[noc_index]] = i
+            i = i + 1
+    for key in noc_dict:
+        writer.writerow([noc_dict[key], key])
+   
+def create_games_csv(reader_list):
+    writer = csv.writer(open('games.csv','w'))
+    game_dict = {}
+    games_index = 8
+    city_index = 11
+    i = 1
+    for row in reader_list[1:]:
+        if row[games_index] not in game_dict:
+            game_dict[row[games_index]] = [i, row[city_index]]
+            i = i + 1
+    
+    for key in game_dict:
+        game_list = key.split()
+        writer.writerow([game_dict[key][0], key, game_list[0], game_list[1], game_dict[key][1]])    
+
+def create_athlete_stats_csv(reader_list):
+    writer = csv.writer(open('athlete_stats.csv','w'))
+    games_reader_list = list(csv.reader(open('games.csv')))
+    games_id_dict = get_id_dict(games_reader_list)
+    athlete_stats_set = set()
+    id_index = 0
+    sex_index = 2
+    age_index = 3
+    height_index = 4
+    weight_index = 5
+    games_index = 8
+
+    for row in reader_list[1:]:
+        games_id = games_id_dict[row[games_index]]
+        athlete_id = int(row[id_index])
+        athlete_stats_set.add((athlete_id, games_id, row[sex_index], row[age_index], row[height_index], row[weight_index]))
+    
+    for stats in athlete_stats_set:
+        writer.writerow([stats[0], stats[1], stats[2], stats[3], stats[4], stats[5]])
+    
+
+
+def create_results_csv(reader_list):
+    writer = csv.writer(open('results.csv','w'))
+    
+    games_reader_list = list(csv.reader(open('games.csv')))
+    games_id_dict = get_id_dict(games_reader_list)
+
+    teams_reader_list = list(csv.reader(open('teams.csv')))
+    teams_id_dict = get_id_dict(teams_reader_list)
+
+    noc_reader_list = list(csv.reader(open('nocs.csv')))
+    noc_id_dict = get_id_dict(noc_reader_list)
+
+    sports_reader_list = list(csv.reader(open('sports.csv')))
+    sports_id_dict = get_id_dict(sports_reader_list)
+
+    event_reader_list = list(csv.reader(open('events.csv')))
+    event_id_dict = get_id_dict(event_reader_list)
+
+    athlete_id_index = 0
+    team_index = 6
+    noc_index = 7
+    games_index = 8
+    sport_index = 12
+    event_index = 13
+    medal_index = 14
+
+    i = 1
+
+    for row in reader_list[1:]:
+        writer.writerow([
+            i,
+            row[athlete_id_index],
+            teams_id_dict[row[team_index]],
+            noc_id_dict[row[noc_index]],
+            games_id_dict[row[games_index]],
+            sports_id_dict[row[sport_index]],
+            event_id_dict[row[event_index]],
+            row[medal_index] ])
+        
+        i = i + 1
+
+def get_id_dict(reader_list):
+    id_dict = {}
+    for row in reader_list:
+        id_dict[row[1]] = row[0]
+    return id_dict
+
+
+
+
+def main():
+    reader_list = list(csv.reader(open('athlete_events.csv', 'r')))
+    create_athletes_csv(reader_list)
+    create_sports_csv(reader_list)
+    create_events_csv(reader_list)
+    create_teams_csv(reader_list)
+    create_nocs_csv(reader_list)
+    create_games_csv(reader_list)
+    create_athlete_stats_csv(reader_list)
+    create_results_csv(reader_list)
+
+if __name__ == '__main__':
+    main()
+
+
---
+++ database/database-schema.sql
@@ -0,0 +1,55 @@
+CREATE TABLE athletes (
+    id SERIAL,
+    athlete_name text
+);
+
+CREATE TABLE teams (
+    id SERIAL,
+    team_name text
+);
+
+CREATE TABLE events (
+    id SERIAL,
+    event_name text
+);
+
+
+CREATE TABLE games (
+    id SERIAL,
+    games text,
+    year integer,
+    season text,
+    city text
+);
+
+
+CREATE TABLE nocs (
+    id SERIAL,
+    noc text
+);
+
+CREATE TABLE results (
+    id SERIAL,
+    athlete_id integer,
+    team_id integer,
+    noc_id integer,
+    games_id integer,
+    sport_id integer,
+    event_id integer,
+    medal text
+);
+
+
+CREATE TABLE sports (
+    id SERIAL,
+    sport_name text
+);
+
+CREATE TABLE athlete_stats (
+	athlete_id integer,
+	games_id integer,
+	sex text,
+	age integer,
+	height integer,
+	weight decimal(5,1)
+);
