48ecaaa9299d4abefb3bb7a8d1ae6bc156961258
Nick Pandelakis <pandelakisn@carleton.edu>
2021-01-28 12:16:41 -0600

    Finished functionality

--- database/olympics.py
+++ database/olympics.py
@@ -18,14 +18,13 @@ def connect_to_database():
 
 def get_parsed_arguments():
     '''Obtains and parses the command line arguments'''
-    with open('.prolog.txt', 'r') as prolog, open('.epilog.txt', 'r') as epilog:
-        parser = argparse.ArgumentParser(description = prolog.read(), epilog = epilog.read())
+    parser = argparse.ArgumentParser()
     
-    parser.add_argument("-n", "--noc", help="One or more 3-letter NOC's to list the athletes for")
+    parser.add_argument("-n", "--noc", help="A 3-letter NOC's to list the athletes for")
 
     parser.add_argument("-l", "--list", action = 'store_true', help="List the number of gold medals won by each NOC in descending order")
 
-    parser.add_argument("-a", "--athlete", help="Lists a specific athlete, as well as their sex, height, and weight for each Olympic game they competed in.")
+    parser.add_argument("-y", "--year", help="Lists all athletes who won a medal for a given year, including what event they won their medal in and the type of medal won.")
 
     parsed_arguments = parser.parse_args()
     return parsed_arguments
@@ -49,9 +48,15 @@ def query_noc_gold_medals(cursor):
         print(e)
         exit()
 
-def query_athlete_stats(athlete, cursor):
+def query_year_medalists(year, cursor):
     try:
-        
+        query_int = int(year)
+        query = '''SELECT athlete_name, event_name, medal FROM athletes LEFT JOIN results ON athletes.id = results.athlete_id JOIN games ON games_id = games.id JOIN events ON results.event_id = events.id WHERE games.year = %s AND results.medal IS NOT NULL;'''
+        cursor.execute(query, (query_int,))
+        return cursor
+    except Exception as e:
+        print(e)
+        exit()
 
 
 
@@ -77,8 +82,11 @@ def print_noc_gold_medals(cursor):
         print(row[0], row[1])
     print()    
 
-
-
+def print_year_medalists(year, cursor):
+    print('===== All medalists in the year ' + year + ' =====')
+    for row in cursor:
+        print(row[0] + ',' + row[1] + ',' + row[2])
+    print()
 
 def main():
     arguments = get_parsed_arguments()
@@ -91,6 +99,11 @@ def main():
     if arguments.list:
         noc_gold_medals = query_noc_gold_medals(cursor)
         print_noc_gold_medals(noc_gold_medals)
-    if arguments.athlete:
-        athlete_stats = query_athlete_stats(arguments, cursor)
-        print_athlete_stats(arguments.athlete, athlete_stats) if __name__ == '__main__': main()
+    if arguments.year:
+        year_medalists = query_year_medalists(arguments.year, cursor)
+        print_year_medalists(arguments.year, year_medalists)
+    
+    connection.close()
+
+if __name__ == '__main__':
+    main()
