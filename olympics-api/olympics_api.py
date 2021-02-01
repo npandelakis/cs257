222c3a7e9724d1cd073d132d69c8daaa53604bab
Nick Pandelakis <pandelakisn@carleton.edu>
2021-02-01 17:09:35 -0600

    Added noc functionality

--- olympics-api/olympics_api.py
+++ olympics-api/olympics_api.py
@@ -10,14 +10,20 @@ import sys
 import argparse
 import flask
 import json
-
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from config import user, database, password
 
 app = flask.Flask(__name__)
 
-
+def connect_to_database():
+    try:
+        connection = (psycopg2.connect(database = database, user = user, password = password))
+        return connection
+    except Exception as e:
+        print(e)
 
 @app.route('/games')
-
 def each_olympics_list():
     olympics_list = []
     ID = flask.request.args.get('id')
@@ -28,22 +34,42 @@ def each_olympics_list():
 
 
 @app.route('/nocs')
-
-def each_NOC_list():
-    pass
-
+def each_NOC_list(): 
+    try:
+        connection = connect_to_database()
+        cursor = connection.cursor(cursor_factory = RealDictCursor)
+        query = '''SELECT noc, noc_region FROM nocs;'''
+        cursor.execute(query)
+        results = cursor.fetchall()
+        return json.dumps(results)
+    except Exception as e:
+        print(e)    
 
 @app.route('/medalists/games/<games_id>?[noc=noc_abbreviation]')
 
 def medalist_list():
-  '''
-  returns first matching games ID
-  '''  
-  
-  actor_dictionary = {}
-  lower_last_name = last_name.lower()
-  for actor in actors:
+    '''returns first matching games ID'''  
+    actor_dictionary = {}
+    lower_last_name = last_name.lower()
+    for actor in actors:
         if actor['last_name'].lower().startswith(lower_last_name):
             actor_dictionary = actor
             break
     return json.dumps(actor_dictionary)
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(host = 'localhost', port = 5000, debug = True)
+
+
+
+
+
+
+
