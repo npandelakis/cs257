48122c838452da6c5e293fb5f5ec30394417ac79
Samuel Gloss <samuelgloss@eduroam-179-92.dyn.carleton.edu>
2021-02-01 21:33:17 -0600

    added /games and /medalists

--- olympics-api/olympics_api.py
+++ olympics-api/olympics_api.py
@@ -10,27 +10,50 @@ import sys
 import argparse
 import flask
 import json
-
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from config import user, database, password
 
 app = flask.Flask(__name__)
+
+def connect_to_database():
+    try:
+        connection = (psycopg2.connect(database = database, user = user, password = password))
+        return connection
+    except Exception as e:
+        print(e)
 
 
 
 @app.route('/games')
 
 def each_olympics_list():
-    olympics_list = []
-    ID = flask.request.args.get('id')
-    year = flask.request.args.get('year')
-    season = flask.request.args.get('season')
-    city = flask.request.args.get('city')
+    '''creates a dictionary of lists of each olympic games'''
+    try:
+        connection = connect_to_database()
+        cursor = connection.cursor(cursor_factory = RealDictCursor)
+        query = '''SELECT id, year, season, city FROM games;'''
+        cursor.execute(query)
+        results = cursor.fetchall()
+        return json.dumps(results)
+    except Exception as e:
+        print(e)    
+    
 
 
 
 @app.route('/nocs')
 
 def each_NOC_list():
-    pass
+    try:
+        connection = connect_to_database()
+        cursor = connection.cursor(cursor_factory = RealDictCursor)
+        query = '''SELECT noc, noc_region FROM nocs;'''
+        cursor.execute(query)
+        results = cursor.fetchall()
+        return json.dumps(results)
+    except Exception as e:
+        print(e)    
 
 
 @app.route('/medalists/games/<games_id>?[noc=noc_abbreviation]')
@@ -39,11 +62,24 @@ def medalist_list():
   '''
   returns first matching games ID
   '''  
+  ID = flask.request.args.get('id')
+  NOC = flask.request.args.get('noc')
+  results = make_dictionaries(ID, 'Gold', NOC)
+  results += make_dictionaries(ID, 'Silver', NOC)
+  results += make_dictionaries(ID, 'Bronze', NOC)
+  return json.dumps(results)
   
-  actor_dictionary = {}
-  lower_last_name = last_name.lower()
-  for actor in actors:
-        if actor['last_name'].lower().startswith(lower_last_name):
-            actor_dictionary = actor
-            break
-    return json.dumps(actor_dictionary)
+  
+def make_dictionaries(ID, medal, noc):
+    '''
+    makes the dictionaires where athlete_id, athlete_name, athlete_sex, sport, event, medal
+    '''
+    try:
+        connection = connect_to_database()
+        cursor = connection.cursor(cursor_factory = RealDictCursor)
+        query = '''SELECT results.athlete_id, , athletes.athlete_name, athlete_stats.sex, sports.sport_name, events.events_name, results.medal FROM results, athletes, athlete_stats, sports, events WHERE events.event_id = {} AND results.medal = {} AND teams.team_name = {}'''.format(ID,medal,noc)
+        cursor.execute(query)
+        results = cursor.fetchall()
+        return results
+    except Exception as e:
+        print(e)    
