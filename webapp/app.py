ab9b916d14d89155e2cfe1e2f9fa4841c7eb63ed
Nick Pandelakis <pandelakisn@carleton.edu>
2021-02-22 21:28:44 -0600

    connected everything up :)

--- webapp/app.py
+++ webapp/app.py
@@ -13,6 +13,7 @@ import flask
 import api
 import psycopg2
 import json
+from flask import request
 from config import user
 from config import password
 from config import database
@@ -32,28 +33,29 @@ def connect_to_database():
 def home():
     return flask.render_template('world-map.html')
 
-# This route supports relative links among your web pages, assuming those pages
-# are stored in the templates/ directory or one of its descendant directories,
-# without requiring you to have specific routes for each page.
-def get_world_data ():
-    connection = connect_to_database()
-    cursor = connection.cursor()
-    query = "SELECT country_attacks_per_year.country_id, country_attacks_per_year.year, country_attacks_per_year.number_of_attacks, countries.country_name FROM country_attacks_per_year, countries WHERE country_attacks_per_year.country_id = countries.id;"
-    cursor.execute(query)
-    country_list = []
-    for row in cursor:
-      one_country = {}
-      one_country['id'] = int(row[0])
-      one_country['year'] = int(row[1])
-      one_country['number_of_attacks'] = row[2]
-      one_country['country_name'] = row[3]
-      country_list.append(one_country)
-    return country_list
-
 @app.route('/world')
 def get_world():
-    country_list = get_world_data()
+    start_year = request.args.get('start_year', default = 1960)
+    end_year = request.args.get('end_year', default = 2018)
+    country_list = get_world_data(start_year, end_year)
     return json.dumps(country_list)
+
+def get_world_data (start_year,end_year):
+    connection = connect_to_database()
+    cursor = connection.cursor()
+    #query = "SELECT country_attacks_per_year.country_id, country_attacks_per_year.year, country_attacks_per_year.number_of_attacks, countries.country_name FROM country_attacks_per_year, countries WHERE country_attacks_per_year.country_id = countries.id;"
+    query = '''SELECT country_name, country_codes, sum(number_of_attacks) 
+            FROM country_attacks_per_year AS c JOIN countries ON c.country_id = countries.id 
+            WHERE countries.country_codes IS NOT NULL AND c.year >= %s  AND c.year <= %s 
+            GROUP BY (country_name, country_codes);'''
+    cursor.execute(query, (start_year, end_year))
+    country_dict = {}
+    for row in cursor:
+      country_dict[row[1]] = {'country_name' : row[0], 'number_of_attacks' : int(row[2]), 'fillColor' : '#F48FB1'}
+      #one_country['country_code'] = row[1]
+      #one_country['number_of_attacks'] = int(row[2])
+      #country_list.append(one_country)
+    return country_dict
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('A tiny Flask application, including API')
---
+++ webapp/static/world-map.js
@@ -0,0 +1,72 @@
+/*
+ *
+ *
+ *
+ *
+ *
+ */
+
+window.onload = initialize;
+
+function initialize() {	
+	initializeMap();
+}
+
+async function initializeMap() {
+	
+	const mapData = await getTerrorismData();
+
+	var map = new Datamap({
+		element: document.getElementById('map-container'),
+		scope: 'world',
+		projection: 'equirectangular',
+		done: onMapDone,
+		data: mapData,
+		fills: { defaultFill:'#999999' },
+		geographyConfig: {
+			popupTemplate: hoverPopupTemplate,
+			borderColor: '#eeeeee',
+			higlightFillColor: '#99dd99',
+			highlightBorderColor: '#000000'
+		}
+
+	});
+}
+
+function onMapDone(dataMap) {
+	dataMap.svg.selectAll('.datamaps-subunit').on('click', onCountryClick);
+}
+
+
+async function getTerrorismData(callback) {
+	var start_year = document.getElementById('start_year');
+	var end_year = document.getElementById('end_year')
+	//var url = getAPIBaseURL() + '/world?start_year=' + start_year
+	//	  + '&end_year =' + end_year
+	var url = 'http://localhost:5000/world'
+
+	const response = await fetch(url);
+
+	return response.json();
+	
+}
+
+function hoverPopupTemplate(geography, data) {
+	var attacks = 0;
+	if (data && 'number_of_attacks' in data) {
+		attacks = data.number_of_attacks;
+	}
+	
+	var template = '<div class = "hoverpopup"><strong>' + geography.properties.name + '</strong><br>\n'
+					+ '<strong>Terrorist Attacks: </strong>' + attacks + '<br>\n' + '</div>';
+	
+	return template;
+}
+
+function onCountryClick(geography) {
+//	window.location = getAPIBaseUrl + '/' + geography.properties.name '/';
+}
+
+function getAPIBaseUrl() {
+	
+}
